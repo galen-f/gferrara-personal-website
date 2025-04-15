f317e0eac4177f47a71b59c1c9d76fcabdcd4c3e
Galen Ferrara <galen.ferrara557@gmail.com>
2025-04-15 19:00:59 +0100

    Added Main Page and placeholde rother pages

--- personal-website-app/src/App.js
+++ personal-website-app/src/App.js
@@ -10,8 +10,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact component=
-          {Home} />
+          <Route path='/' exact element=
+          {<Home />} />
         </Routes>
       </Router>
     </>
---
+++ personal-website-app/src/components/Cards.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
+
+function Cards() {
+  return (
+    <div className="cards">
+        <h1>Check out these awesome projects!</h1>
+        <div className="cards__container">
+            <div className="cards__wrapper">
+                <div className="cards__items">
+
+                    <CardItem 
+                    src="images/img-9.jpg"
+                    text="Lavender Launcher"
+                    label="Kotlin"
+                    path='/portfolio'
+                    />
+
+                    <CardItem 
+                    src="images/img-8.jpg"
+                    text="Pebble - Photo Organization"
+                    label="Python"
+                    path='/portfolio'
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
