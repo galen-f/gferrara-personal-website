d74a25ee7c7d82b50f72b53c86fb1c1ea36be05f
Galen Ferrara <galen.ferrara557@gmail.com>
2025-05-24 17:35:12 +0100

    - Fix for PFP - Lavender demo for mobile fix - Added copyright notation

--- personal-website-app/src/components/Footer.js
+++ personal-website-app/src/components/Footer.js
@@ -7,7 +7,10 @@ function Footer() {
         <section className="footer-subscription">
             <p className="footer-subscription-heading">
                 ferraragalen@gmail.com - +44 7918 577 677 - +1 (845) 798 9409   
-            </p>  
+            </p>
+            <p className="footer-subscription-heading">
+                Copyright © 2025 - Present. Galen John Ferrara. All rights reserved.  
+                </p>
         </section>
     </div>
   )
